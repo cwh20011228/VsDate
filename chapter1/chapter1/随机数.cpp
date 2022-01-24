07bc5671dd23d34822ea053d9c46d278e9883938
陈伟浩 <cwh30456966670cwh@gmail.com>
2022-01-24 20:39:29 +0800

    change

--- chapter1/chapter1/随机数.cpp
+++ chapter1/chapter1/随机数.cpp
@@ -5,14 +5,17 @@ using namespace std;
 
 int main(void)
 {
-    const int MIN_VALUE = 10;
-    const int MAX_VALUE = 18;
+    const int MIN_VALUE = 10; // 最小值
+    const int MAX_VALUE = 18; // 最大值
+
     unsigned seed; //  保存种子的变量
     cout << "Enter a seed value:  ";
+
     seed = time(0); // 必须给它传递一个0
-    // cin>>seed;
+
     srand(seed);                                                //  srand() 提供一个种子
     int num = rand() % (MAX_VALUE - MIN_VALUE + 1) + MIN_VALUE; // num 范围是(MIN_VALUE ~ MAX_VALUE)
+
     for (int i = 0; i < 6; i++)
     {
         cout << num << "  ";
